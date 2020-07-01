a93062b2796e177ec9ec1ba09a65303a6627b33a
Matheus Reis <matheus.reis@olist.com>
2020-07-01 16:16:43 -0300

    Edited method of insert data into json file

--- db/product_db.py
+++ db/product_db.py
@@ -4,9 +4,23 @@ class ProductDB:
     def __init__(self):
         self.__data = {"products": []}
 
+    def get(self):
+        with open('products.txt') as file:
+            data = json.load(file)
+
+        return data["products"]
+
+    def get_all(self):
+        with open('products.txt') as file:
+            data = json.load(file)
+
+        return data
+
     def insert(self, product):
-        with open('products.txt', 'w') as file:
-            self.__data["products"].append(product.serialize())
+        self.__data = self.get_all()
+        self.__data["products"].append(product.serialize())
+
+        with open('products.txt', 'w+') as file:
             json.dump(self.__data, file, indent=4)
 
     def insert_list(self, products):
@@ -16,8 +30,3 @@ class ProductDB:
             print(self.__data)
             json.dump(self.__data, file, indent=4)
 
-    def get(self):
-        with open('products.txt') as file:
-            data = json.load(file)
-
-        return data["products"]
